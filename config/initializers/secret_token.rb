62baed48ee957b40e21bac5437058ae6015ceb48
Joel Hughes <jh@jojet.com>
2013-03-02 20:33:15 +0000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joeltest::Application.config.secret_token = '32f003347e77ec6d78537faebe246f8fdaf9cdb12daa70189f290d16e63b05071b91394b325bd6445f58b130c8dd8962ae249b51e3f6d38b065eae047afbb6c1'
